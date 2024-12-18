a5188a6544d9b1c1eece50cbd773415c648787cf
Damzyd1 <shittud601@gmail.com>
2024-12-18 15:20:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ let button = document.getElementById("button");
 password.addEventListener("keyup", () => {
   button.disabled = !password.value
 });
-
-let users = ["David", "Ola", "Samuel"];
+button.addEventListener("click", function(){
+  login();
+});
+/*let users = ["David", "Ola", "Samuel"];
 function goToNextPage(){
     for(i=0; i < users.length; i++){
       if(password.value == users[i]){
@@ -22,11 +24,52 @@ function goToNextPage(){
   }
 }
 
-document.addEventListener("keydown", e => {
+*/
+
+const validPasswords = ["Damilola", "Ayomide", "Shittu"];
+const passwordVersion = "v1";
+const sessionExpiryTime = 1 * 60  * 1000;
+
+function login(){
+  const enteredPassword = password.value;
+  if(validPasswords.includes(enteredPassword)){
+    const currentTime = new Date().getTime();
+    localStorage.setItem("isLoggedIn", true);
+    localStorage.setItem("passwordVersion",passwordVersion);
+    localStorage.setItem("loginTimeStamp",currentTime);
+    setTimeout(delay,3000);
+      function delay(){
+      window.location.href = "cgpa.html";
+  }
+  
+  }else{
+    alert("Invalid password, Please try again.")
+  }
+}
+
+
+function protectPage(){
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  const savedVersion = localStorage.getItem("passwordVersion");
+  const loginTimeStamp = localStorage.getItem("loginTimeStamp");
+  const currentTime = new Date().getTime();
+  
+  if(
+      !isloggedIn ||
+      savedVersion !== passwordVersion ||
+      (currentTime - loginTimeStamp) > sessionExpiryTime
+    ){
+      alert("Session expired or invalid. Please log in again.");
+      localStorage.clear();
+      window.location.href = "index.html";
+    }
+}
+/*document.addEventListener("keydown", e => {
   if(e.key = "Enter"){
-    goToNextPage();
+    login();
   }
 });
+*/
 
 let countSystem;
   let colors = ["#5F9EA0","#1C39BB","#082567","#082567"];
