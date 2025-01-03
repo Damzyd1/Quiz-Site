b1e4c8fb7cd8552456ccd721f6007613ee433338
Damzyd1 <shittud601@gmail.com>
2025-01-03 17:35:57 +0100

    Update showQuestion.js

--- showQuestion.js
+++ showQuestion.js
@@ -177,9 +177,247 @@ let availableQuestion = [];
         choice3: "Particles of liquid",
         choice4: "Entity",
         answer: "Atom"
+    },
+    {
+        question: "______ phenomenon keeps the earth warm enough to sustain life on earth",
+        choice1: "The green gases effect",
+        choice2: "The greenhouse effect",
+        choice3: "The green bomb effect",
+        choice4: "Global warming effect",
+        answer: "The greenhouse effect"
+    },
+    {
+        question: "The heat energy released by fire is propagated by",
+        choice1: "Conductor, electric, conversion",
+        choice2: "Conduction, convection and radiation",
+        choice3: "Radiation, conducive, propagation",
+        choice4: "Global warming, conduction, elastic",
+        answer: "Conduction, convection and radiation"
+    },
+    {
+        question: "____ is everything around us",
+        choice1: "Atmosphere",
+        choice2: "Environment",
+        choice3: "Chemical",
+        choice4: "Air",
+        answer: "Environment"
+    },
+    {
+        question: "There are ____ Environmental realms or segments",
+        choice1: "4",
+        choice2: "5",
+        choice3: "3",
+        choice4: "2",
+        answer: "4"
+    },
+    {
+        question: "The Environmental segments are",
+        choice1: "Lithosphere, antrosphere, sociosphere",
+        choice2: "Lithosphere, hydrosphere, biosphere, atmosphere",
+        choice3: "Osyshere, Oxysphere lithosphere, atmosphere",
+        choice4: "Hydrosphere, lithosphere, bargosphere, nitrosphere",
+        answer: "Lithosphere, hydrosphere, biosphere, atmosphere"
+    },
+    {
+        question: "The lithosphere consists of",
+        choice1: "Upper area and crest",
+        choice2: "Lower core and crust",
+        choice3: "Upper mantle and sand",
+        choice4: "Upper mantle and crust",
+        answer: "Upper mantle and crust"
+    },
+    {
+        question: "____ is the earth's outer skin that is accessible to humans",
+        choice1: "Upper mantle",
+        choice2: "Core",
+        choice3: "Crust",
+        choice4: "Mantle",
+        answer: "Crust"
+    },
+    {
+        question: "What is the major setback for global water supply?",
+        choice1: "Lack of accessibility to water",
+        choice2: "Non-uniform distribution",
+        choice3: "Non-availability of fresh water",
+        choice4: "Lack of natural resources",
+        answer: "Non-uniform distribution"
+    },
+    {
+        question: "The biosphere is divided into smaller units called",
+        choice1: "Atmosphere",
+        choice2: "Lithium",
+        choice3: "Ecosystem",
+        choice4: "Biosystem",
+        answer: "Ecosystem"
+    },
+    {
+        question: "The atmosphere is comprised of how many layers?",
+        choice1: "4",
+        choice2: "3",
+        choice3: "5",
+        choice4: "2",
+        answer: "4"
+    },
+    {
+        question: "The region at about 500km of the earth's surface is known as",
+        choice1: "Ecosphere",
+        choice2: "Ecosystem",
+        choice3: "Exosphere",
+        choice4: "Atmosphere",
+        answer: "Exosphere"
+    },
+    {
+        question: "The atmosphere layers include all except",
+        choice1: "Lithosphere",
+        choice2: "Troposphere",
+        choice3: "Stratosphere",
+        choice4: "Mesosphere",
+        answer: "Lithosphere"
+    },
+    {
+        question: "What acts as a source of Carbon dioxide for plant photosynthesis and oxygen for respiration?",
+        choice1: "Biosphere",
+        choice2: "Atmosphere",
+        choice3: "Lithosphere",
+        choice4: "Mesosphere",
+        answer: "Atmosphere"
+    },
+    {
+        question: "What does the earth atmosphere look like?",
+        choice1: "Round",
+        choice2: "A thick blue veil",
+        choice3: "A thin rope",
+        choice4: "A thin blue veil",
+        answer: "A thin blue veil"
+    },
+    {
+        question: "According to Stern, 1997, Earth has a mass of about ____ metric tons",
+        choice1: "5.13×10",
+        choice2: "5.15×10",
+        choice3: "5.12×10",
+        choice4: "5.11×10",
+        answer: "5.15×10"
+    },
+    {
+        question: "The three main gaseous elements that account for about 99.9% of the earth are",
+        choice1: "Nitrogen, Oxygen and Argon",
+        choice2: "Nitrogen, oxygen and lithium",
+        choice3: "Sodium, Carbon and Argon",
+        choice4: "Sulphur, Nitrogen and Oxygen",
+        answer: "Nitrogen, Oxygen and Argon"
+    },
+    {
+        question: "According to Stanley, 1995, the concentration by volume of the three main gaseous elements are",
+        choice1: "75.09%, 20.95%, 0.95%",
+        choice2: "78.09%, 22.95%, 0.93%",
+        choice3: "78.09%, 20.95%, 0.93%",
+        choice4: "75.09%, 22.95%, 0.95%",
+        answer: "78.09%, 20.95%, 0.93%"
+    },
+    {
+        question: "___ is defined as the number of molecules of the trace gas divided by the total number of molecules present in the volumes sampled",
+        choice1: "Molecule",
+        choice2: "Ratio",
+        choice3: "Proportion",
+        choice4: "Probability",
+        answer: "Ratio"
+    },
+    {
+        question: "Ppmv stands for",
+        choice1: "Per million by volume",
+        choice2: "Percent of minute by volume",
+        choice3: "Probability of million by volts",
+        choice4: "Probably millions of voltage",
+        answer: "Per million by volume"
+    },
+    {
+        question: "Ppbv stands for",
+        choice1: "Probably billions of Voltage",
+        choice2: "Percent of billions by volume",
+        choice3: "Per billions by volume",
+        choice4: "Probability of billions by volts",
+        answer: "Per billions by volume"
+    },
+    {
+        question: "Food according to Lapedes 1997 is",
+        choice1: "The material that enabled man to grow and reproduce himself",
+        choice2: "The material that is necessary for health, growth, and normal function of living organisms",
+        choice3: "As anything that eliminates hunger",
+        answer: "The material that is necessary for health, growth, and normal function of living organisms"
+    },
+    {
+        question: "The important aspect of food security issues to be considered according to Honfoga & Van den Boom, 2003 include the following except",
+        choice1: "Availability of foodstuff",
+        choice2: "The quality of the diet",
+        choice3: "Ability to satisfy hunger",
+        choice4: "The stability of supplies over time and space",
+        choice5: "Access to food produced",
+        answer: "Ability to satisfy hunger"
+    },
+    {
+        question: "According to the passage, how much crude protein per day is recommended for an individual to consume?",
+        choice1: "65-86g",
+        choice2: "68-56g",
+        choice3: "45-86g",
+        choice4: "59-86g",
+        answer: "65-86g"
+    },
+    {
+        question: "Based on Wiebe, 2003; FAO, 2005, more than how many people are food insecure?",
+        choice1: "Seven hundred million people",
+        choice2: "Six hundred million people",
+        choice3: "Eight hundred million people",
+        choice4: "Four hundred million people",
+        answer: "Eight hundred million people"
+    },
+    {
+        question: "The prevalence of human malnutrition remains high in",
+        choice1: "Sub-Saharan Africa and South Asia",
+        choice2: "West Africa and Asia",
+        choice3: "South Africa and South Asia",
+        answer: "Sub-Saharan Africa and South Asia"
+    },
+    {
+        question: "According to the passage, ___ has been identified as the principal cause of increasing and accelerated migration from rural to urban areas in developing countries.",
+        choice1: "Air pollution",
+        choice2: "Food insecurity",
+        choice3: "Nuclear changes",
+        choice4: "Nutritional defects",
+        answer: "Food insecurity"
+    },
+    {
+        question: "The root problem of inadequate access to food is",
+        choice1: "Unemployment",
+        choice2: "Lack of income",
+        choice3: "Poverty",
+        choice4: "Inadequate farm produce",
+        answer: "Poverty"
+    },
+    {
+        question: "Solutions to food insecurity according to Jayne et al., 1994 include the following except",
+        choice1: "Non-market distribution of aid",
+        choice2: "Given existing market costs",
+        choice3: "Reducing the cost of food delivered through markets by fostering technical and institutional innovations in farm-level production and the marketing system",
+        choice4: "Enhancing the means to acquire food",
+        answer: "Enhancing the means to acquire food"
+    },
+    {
+        question: "On the basis of spoilage, food is divided into",
+        choice1: "4 categories",
+        choice2: "3 categories",
+        choice3: "5 categories",
+        choice4: "2 categories",
+        answer: "3 categories"
+    },
+    {
+        question: "____ is the act of protecting food from deterioration and decay so that it will be available for future consumption.",
+        choice1: "Food consumption",
+        choice2: "Food security",
+        choice3: "Food preservation",
+        choice4: "Food storage",
+        answer: "Food preservation"
     }
 ];
-
 /*const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 4;
 
