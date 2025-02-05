1965500d27f4516866616805c8bf3c9a9bb483d4
Damzyd1 <shittud601@gmail.com>
2025-02-05 21:24:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function goToNextPage(){
 
 */
 
-const validPasswords = ["Eniola@Prime21","Hemy1","Seas1","Ramzy","Aminatu","Seyi#Pro21","Jola1","Fati1","Damilola","Ayopartner","Salam1","Jaysay","Giwa#exec","Freeman1","Otal!2024","Kanyestyles1","Sara#2024","Raphael#Pro","Ys@Pro2025","Yusuf#Elite","Ayoman1","Fikayo@Corp","Adamu$Safe","J@mes2025","Bashir#Biz"];
-const passwordVersion = "v20";
+const validPasswords = ["Eniola@Prime21","Hemy1","Jemimes","Seas1","Ramzy","Aminatu","Seyi#Pro21","Jola1","Fati1","Damilola","Ayopartner","Salam1","Jaysay","Giwa#exec","Freeman1","Otal!2024","Kanyestyles1","Sara#2024","Raphael#Pro","Ys@Pro2025","Yusuf#Elite","Ayoman1","Fikayo@Corp","Adamu$Safe","J@mes2025","Bashir#Biz"];
+const passwordVersion = "v21";
 const sessionExpiryTime = 1 * 60 * 60 * 1000; // 1 minute
 const splashScreen = document.getElementById("splash-screen");
 
@@ -58,7 +58,7 @@ function login() {
 
 // Protect Page Function
 function protectPage() {
-  const passwordVersion = "v20";
+  const passwordVersion = "v21";
   const sessionExpiryTime = 1 * 60 * 60 * 1000; // 1 minute
 
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true"; // Ensure type check
